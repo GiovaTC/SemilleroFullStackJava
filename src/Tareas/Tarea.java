01100a09a129b9fe562d9af72c06ba53a0c9c6b7
Giovanny <giovanny.tapiero@pi.edu.co>
2023-09-29 12:16:09 -0500

    Initial commit

---
+++ src/Tareas/Tarea.java
@@ -0,0 +1,11 @@
+public class Tarea {
+    private String descripcion;
+
+    public Tarea(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
